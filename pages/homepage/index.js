3fed5f5b2647518306b51db948e970d5127b0a4f
Mac <mac@Macs-MacBook-Pro.local>
2021-05-24 12:36:27 +0100

    about us and why choose us sections

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -6,6 +6,51 @@ menuToggle.addEventListener('click', () => {
   showcase.classList.toggle('active');
 })
 
+// ABOUT US SLIDER
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide() {
+
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+
+  slides[index].style.display = "block";
+  dot[index].classList.add("active");
+
+  index++;
+
+  setTimeout(changeSlide, 2000);
+
+}
+
+changeSlide();
+
+// ABOUT US SLIDER
+
+// ABOUT US SECTION THUMBNAIL SLIDER
+function changeimg(url, e) {
+  document.getElementById("img").src = url;
+  let nodes = document.getElementById("thumb_img");
+  let img_child = nodes.children;
+  for (i = 0; i < img_child.length; i++) {
+    img_child[i].classList.remove('active')
+  }
+  e.classList.add('active');
+  console.log('it works');
+}
+
 
 // SCROLL TO VIEW
 var downBtn = document.querySelector('.scroll-down-btn');
